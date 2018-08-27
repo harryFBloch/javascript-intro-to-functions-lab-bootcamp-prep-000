0231c91ecb54da62a3a462371ad1d496740dc929
Harry Bloch <harryfbloch@gmail.com>
2018-08-27 14:38:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
         return "YES INDEED!"
       }
        if (string == "I love u, Grandma."){
-      return "I love you, too"
+        return "I love you, too"
       }
 }
 
